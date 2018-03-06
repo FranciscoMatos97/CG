8bd582e9e60d23a84f22cb996cce0f862030abb4
tiago <tiago.tdaa@gmail.com>
2018-03-06 17:09:43 +0000

    organized files.

--- src/headers/Point.h
+++ src/headers/Point.h
@@ -1,20 +1,21 @@
-#define __POINT_H__
-
-#include <math.h>
-
-using namespace std;
+#ifndef PROJETO_POINT_H__
+#define PROJETO_POINT_H__
 
 class Point{
-	float x, y, z;
 
-public:
-	Point();
-	Point(float, float, float);
-	float getX();
-	float getY();
-	float getZ();
-	void setX(float);
-	void setY(float);
-	void setZ(float);
+	float x;
+	float y;
+	float z;
 
+	public:
+		Point();
+		Point(float,float,float);
+		float getX();
+		float getY();
+		float getZ();
+		void setX(float);
+		void setY(float);
+		void setZ(float);
 };
+
+#endif
