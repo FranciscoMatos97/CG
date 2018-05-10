2e796f3e2938de6fe6ef220d83572851c3aac03e
sofiapereiraGIT <sofiapereira9@hotmail.com>
2018-05-10 16:50:14 +0100

    Headers atualizados.

--- headers/Parser.h
+++ headers/Parser.h
@@ -12,13 +12,20 @@
 #include "tinyxml2.h"
 #include "Transform.h"
 #include "Struct.h"
+#include "Light.h"
+#include "Material.h"
 
 using namespace std;
 using namespace tinyxml2;
 
+Struct readFile(string, Struct);
+Struct lookUpTranslate(XMLElement*, Struct);
+Struct lookUpTransformation(XMLElement*, Struct);
+Struct lookUpLight(XMLElement*, Struct);
+Struct lookUpMaterial(XMLElement*, Struct);
+Struct lookUpModel(XMLElement*, Struct);
 vector<Struct> lookAux(XMLElement*);
 vector<Struct> lookFiles(char*);
-vector<Point*> readFile(string);
 int parseXML(char*);
 
 #endif
