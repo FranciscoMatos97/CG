56ac99f78c1d949d326295e9cd8bb4b152b497a9
FranciscoMatos97 <franmatos97@gmail.com>
2018-05-10 14:05:13 +0100

    Ficheiros 3d, alguns e generator mesmo Nice

--- headers/vertex.h
+++ headers/vertex.h
@@ -32,7 +32,10 @@ class Vertex{
         Point* bezierCurve(float, Point*, Point*, Point*, Point*);
         Point* bezierPatch(float, float, vector<Point*>);
         vector<Point*> bezierPatchTriangles(int, vector<Patch*>);
-
+        void cross(float*, float*, float*);
+        void normalize(float*);
+        float bezierPointTangent(float u, float v, float m[4][4] , float t[4][4], int d);
+        vector<Point*> bezierTangent(int,vector<Patch*>);
 };
 
 #endif
